d44f8b9c7ed42b7ce69bad1c3318fa996165b5d1
Riyashetti <riyashetti15@gmail.com>
2025-07-01 15:34:14 +0530

    Add files via upload

---
+++ pgm/Printing.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class Father{
+protected String temple="Laxmi Temple";
+protected double land_acr=4;
+}
+class Me extands Father{
+protected double gold_kg=5;
+}
+public class Printing
+{
+public static void main(String []args)
+{
+me ob = new me();
+System.out.println("Temple Name"+ob.temple);
+System.out.println("Land in Acer"+ob.land_acr);
+System.out.println("Gold in Kg"+ob.gog_kg);
+}
+}
